1d0d802ccf1ea8a26313c25cb01fceebbc213b5f
jay <jay@kali.kali>
2024-08-12 09:46:57 -0400

    updates

--- app/main.py
+++ app/main.py
@@ -1,58 +1,62 @@
-from flask import Flask,redirect,url_for,render_template,request
-import youtube_dl
+from flask import Flask, redirect, url_for, render_template, request
+import yt_dlp as youtube_dl  # Use yt-dlp instead of youtube_dl
 import pandas as pd
 import numpy as np
-from lxml.html import fromstring
-import requests
-from itertools import cycle
-import traceback
 
+app = Flask(__name__)
 
-
-
-app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/process',methods = ['POST'])
+@app.route('/process', methods=['POST'])
 def process():
     user_input = request.form["name"]
+
     def my_hook(d):
         if d['status'] == 'finished':
             print('Done downloading, now converting ...')
-    ydl_opts = {
-    'format': 'bestaudio/best',       
-    'outtmpl': '%(id)s',        
-    'noplaylist' : True,        
-    'progress_hooks': [my_hook] 
-    }
-    
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        result= ydl.extract_info(url = user_input,download = False)
-    i =0
-    formats = []
-    extension = []
-    filesize = []
-    link = []
-    while i < len(result["formats"]):
-        formats.append(result["formats"][i]['format_id'])
-        extension.append(result["formats"][i]['ext'])
-        filesize.append(result["formats"][i]['filesize'])
-        link.append(result["formats"][i]['url'])
-        i = i+1
-    df = pd.DataFrame()
-    df["format_id"] = formats
-    df["extension"] = extension
-    df["filesize"] = filesize
-    df["links"] = link
-    df["filesize"] = df["filesize"].apply(lambda x: str(np.trunc(x/1000000)) + "MB")
-    ext = zip(extension,df["filesize"],link)
-    return render_template('index.html',ext=ext) 
 
-@app.route('/download',methods = ['POST'])
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': '%(id)s',
+        'noplaylist': True,
+        'progress_hooks': [my_hook]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(url=user_input, download=False)
+
+    formats = []
+    extensions = []
+    filesizes = []
+    links = []
+    for format in result["formats"]:
+
+        # Filter out formats that are not video files (e.g., storyboards, streams)
+        if format.get('vcodec') != 'none' and format.get('acodec') != 'none':
+            format_id = format.get('format_id')
+            ext = format.get('ext')
+            filesize = format.get('filesize', 0)  # Size might not always be available
+            url = format.get('url')                
+            formats.append(format_id)
+            extensions.append(ext)
+            filesizes.append(filesize)
+            links.append(url)
+
+    df = pd.DataFrame({
+        "format_id": formats,
+        "extension": extensions,
+        "filesize": filesizes,
+        "links": links
+    })
+
+    df["filesize"] = df["filesize"].apply(lambda x: str(np.trunc(x / 1000000)) + "MB" if x else "Unknown")
+    print(df)
+    ext = zip(extensions, df["filesize"], links)
+    return render_template('index.html', ext=ext)
+
+@app.route('/download', methods=['POST'])
 def download():
     print(type(request.form.get('service')))
-    
     return render_template('index.html')
-    
